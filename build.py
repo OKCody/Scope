1dd6ca59237058cd356c2acb4f913acebc1334c0
Cody Taylor <codyalantaylor@gmail.com>
2016-03-16 14:57:40 -0500

    most basic modular structure; creates empty HTML tree and inserts contents of separate file in to main section of <body>

--- build.py
+++ build.py
@@ -1,25 +1,38 @@
-indexContent = open('content/index.html', 'r')
-indexContent = indexContent.read()
-index = open('index.html', 'a')
-	
-#def mkStyle():
+pageContent = open('content/index.html', 'r')
+pageContent = pageContent.read()
+page = open('index.html', 'a')
 
-#def mkHeader():
 
-def mkBody(content):
-   index.write(content)
+    
+def mkFooter():
+    page.write('<footer>\n')
+    page.write('</footer>\n')
+    
+def mkMain(pageContent):
+    page.write(pageContent)
+    page.write('\n')
+    
+def mkHeader():
+    page.write('<header>\n')
+    page.write('</header>\n')
 
-#def mkFooter():
-#-----main-----
-index.write('<html>')
-index.write('<style>')
-index.write('</style>')
-index.write('<head>')
-index.write('</head>')
-index.write('<body>')
-mkBody(indexContent)
-index.write('</body>')
-index.write('</html>')
-#-----/main-----
+def mkHead():
+    page.write('<head>\n')
+    page.write('</head>\n')
+    
+def mkBody(pageContent):
+    page.write('<body>\n')
+    mkHeader()
+    mkMain(pageContent)
+    mkFooter()
+    page.write('</body>\n')
 
-index.close()
+def mkHTML(pageContent):
+    page.write('<html>\n')
+    mkHead()
+    mkBody(pageContent)
+    page.write('</html>\n')
+
+mkHTML(pageContent)
+
+page.close()
