9fa2cbbbac114fac8b6d65396e5e4b40859ac31f
Cody Taylor <codyalantaylor@gmail.com>
2016-03-28 16:47:36 -0500

    added line in build.sh to create <title> metadata using filename as title; still need to replace underscores with spaces in title;

--- site-scripts/build.sh
+++ site-scripts/build.sh
@@ -1,13 +1,19 @@
 #delete site-content directory
 rm -rf site-content
+
 #pull down most current version of all site entries
 git clone http://github.com/OKCody/Pages
+
 #rename cloned directory to fit schema
 mv Pages site-content
-#concatenate contents of head.html, $filename.html, and tail.html and write to
-#file in archive directory
+
 cd site-content
 for filename in *.html
 do
+#concatenate contents of head.html, $filename.html, and tail.html and write to
+#file in archive directory
   cat ../site-template/head.html $filename ../site-template/tail.html > ../content/$filename
+
+#find "<title></title>" in previously created file. replace with <title>$filename</title>
+  sed -i "s/<title><\/title>/<title>$filename<\/title>/g" ../content/$filename
 done
