21695c162a9f9e07f0e40846874a5bdcfd5bf6b3
Cody Taylor <codyalantaylor@gmail.com>
2016-04-03 18:12:19 -0500

    properly removes dates from filenames in /content, but does not concatenate head.html and tail.html to index.html

--- site-scripts/build.sh
+++ site-scripts/build.sh
@@ -5,7 +5,9 @@
 #delete site-content directory, will be recreated when cloned
 #delete contents of /contents
 rm -rf site-content
-rm content/*.html
+rm -rf content
+mkdir content
+rm index.html
 
 #pull down most current version of all site entries
 git clone http://github.com/OKCody/Pages
@@ -19,10 +21,6 @@ do
   #concatenate contents of head.html, $filename.html, and tail.html and write to
   #file in archive directory
   cat ../site-template/head.html $filename ../site-template/tail.html > ../content/$filename
-
-  #find "<title></title>" in previously created file. replace with <title>$filename</title>
-  #replace underscores with spaces and remove ".html" from end of filename
-  sed -i "s/<title><\/title>/<title>${filename//_/ }<\/title>/g ; s/.html<\/title>/<\/title>/g" ../content/$filename
 done
 cd ..
 
@@ -31,16 +29,27 @@ cd ..
 #prepending date to filename in this way forces most recent page to bottom of ls
 index=$(ls content/ | tail -n1)
 cp content/$index index.html
+#concatenate contents of head.html, $filename.html, and tail.html and write to
+#file in archive directory
+cat site-template/head.html index.html site-template/tail.html > index.html
+sed -i "s/<title><\/title>/<title>Cody Taylor<\/title>/g ; s/.html<\/title>/<\/title>/g" index.html
 
-#remove dates from front of filenames in content/
-#from
-#yyyymmmdd_filename.html
-#to
-#filename.html
-#dates on filenames in site-content/ do not matter (not public facing)
 cd content
 for filename in *.html
 do
+  #remove dates from front of filenames in content/
+  #from
+  #yyyymmmdd_filename.html
+  #to
+  #filename.html
+  #dates on filenames in site-content/ do not matter (not public facing)
   mv $filename ${filename:9}
+done
+
+for filename in *.html
+do
+  #find "<title></title>" in previously created file. replace with <title>$filename</title>
+  #replace underscores with spaces and remove ".html" from end of filename
+  sed -i "s/<title><\/title>/<title>${filename//_/ }<\/title>/g ; s/.html<\/title>/<\/title>/g" $filename
 done
 cd ..
