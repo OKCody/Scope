cf005280bccdd84f7403dcf57c29f64b257ae266
Cody Taylor <codyalantaylor@gmail.com>
2016-04-16 11:08:35 -0500

    changed all references to content/ to root/

--- site-scripts/build.sh
+++ site-scripts/build.sh
@@ -7,12 +7,12 @@ echo "Cleaning up Scope/ ..."
 #delete site-content directory, w ill be recreated when cloned
 #delete contents of /contents
 rm -rf site-content
-rm -rf content
-mkdir content
+rm -rf root
+mkdir root
 #-------------------------------------------------------------------------------
 
 #--------------------------Clone pages from GitHub------------------------------
-echo "Cloning Pages/ from Github..."
+echo "Cloning Pages/ from Githgitub..."
 
 #pull down most current version of all site entries
 git clone -q http://github.com/OKCody/Pages
@@ -36,28 +36,28 @@ cd ..
 #---------------------------Home directory prep.--------------------------------
 echo "Copying dependent files into root directory..."
 #copy style and images from Scope/ into content/ to be pushed to server
-mkdir content/style
-mkdir content/site-images
-mkdir content/site-images/license
+mkdir root/style
+mkdir root/site-images
+mkdir root/site-images/license
 
 cd site-style
 for filename in *.css
 do
-  cp $filename ../content/style/$filename
+  cp $filename ../root/style/$filename
 done
 cd ..
 
 cd site-images
 for filename in *.???
 do
-  cp $filename ../content/site-images/$filename
+  cp $filename ../root/site-images/$filename
 done
 cd ..
 
 cd site-images/license
 for filename in *
 do
-  cp $filename ../../content/site-images/license/$filename
+  cp $filename ../../root/site-images/license/$filename
 done
 cd ../..
 #-------------------------------------------------------------------------------
@@ -71,28 +71,28 @@ for filename in *.html
 do
   #concatenate contents of head.html, $filename.html, and tail.html and write to
   #file in archive directory
-  cat ../site-template/head.html $filename ../site-template/tail.html > ../content/$filename
+  cat ../site-template/head.html $filename ../site-template/tail.html > ../root/$filename
 done
 cd ..
 
 #create index.html from most recent file in /content based on date at front of filename
 #yyyymmmdd_filename.html
 #prepending date to filename in this way forces most recent page to bottom of ls
-index=$(ls content/*.html | tail -n1)
-mv $index content/index.html
+index=$(ls root/*.html | tail -n1)
+mv $index root/index.html
 
-sed -i "s/<title><\/title>/<title>Cody Taylor<\/title>/g ; s/.html<\/title>/<\/title>/g" content/index.html
+sed -i "s/<title><\/title>/<title>Cody Taylor<\/title>/g ; s/.html<\/title>/<\/title>/g" root/index.html
 #change path to /site-style as it is different for index.html than it is for all other pages.
-sed -i "s/..\/style\/normalize.css/style\/normalize.css/g" content/index.html
-sed -i "s/..\/style\/skeleton.css/style\/skeleton.css/g" content/index.html
-sed -i "s/..\/style\/style.css/style\/style.css/g" content/index.html
-sed -i "s/..\/style\/print.css/style\/print.css/g" content/index.html
+sed -i "s/..\/style\/normalize.css/style\/normalize.css/g" root/index.html
+sed -i "s/..\/style\/skeleton.css/style\/skeleton.css/g" root/index.html
+sed -i "s/..\/style\/style.css/style\/style.css/g" root/index.html
+sed -i "s/..\/style\/print.css/style\/print.css/g" root/index.html
 #-------------------------------------------------------------------------------
 
 #--------------------Prepare public-facing pages and PDFs-----------------------
 echo "Creating page directories; index.html and print.pdf for each..."
 
-cd content
+cd root
 numfiles=$(find -maxdepth 1 -type f | wc -l)
 currentfile=0
 for filename in [!index.html]*.html
